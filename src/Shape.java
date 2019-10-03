221cbbbf7f9668a7f2fdf935fcdcc9860a48e0cd
NguyenViet07 <nguyenviet.vn.07@gmail.com>
2019-10-03 16:57:20 +0700

    xong

---
+++ src/Shape.java
@@ -0,0 +1,34 @@
+public class Shape {
+    public String color = "red";
+    public boolean fill = true;
+    public Shape(){
+    }
+
+    public Shape(String color, boolean fill){
+        this.color = color;
+        this.fill = fill;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFill() {
+        return fill;
+    }
+
+    public void setFill(boolean fill) {
+        this.fill = fill;
+    }
+    public String toString(){
+        if (this.fill){
+            return "A Shape with color" + this.color + "and filled";
+        } else {
+            return "A shape with color" + this.color + "and not filled";
+        }
+    }
+}
